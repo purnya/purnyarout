868313ab23e0a0f6390b5e9888488c19fc3d4561
Purnya Rout <purnya.rout09@gmail.com>
2016-04-20 18:00:56 +0530

    ADD POST for todos

--- server.js
+++ server.js
@@ -1,57 +1,27 @@
-// Example chapter1
-
-/*var express = require('express');
-var app = express();
-var PORT = process.env.PORT || 3000;
-
-app.get('/', function (req, res) {
-	res.send('Todo API Root');
-});
-
-app.listen(PORT, function () {
-	console.log('Express listening on port ' + PORT + '!');
-});*/
-
-
-// Example chapter2 getting all todos/ todo by Id
 var express = require('express');
+var bodyParser = require('body-parser');
+var _ = require('underscore');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
+var todos = [];
+var todoNextId = 1;
 
-var todos = [{
-	id: 1,
-	description: 'Meet mom for lunch',
-	completed: false
-}, {
-	id: 2,
-	description: 'Go to market',
-	completed: false
-}, {
-	id: 3,
-	description: 'Feed the cat',
-	completed: true
-}];
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
 	res.send('Todo API Root');
 });
 
-// request : GET , Url : /todos
+// GET /todos
 app.get('/todos', function (req, res) {
 	res.json(todos);
 });
 
 // GET /todos/:id
 app.get('/todos/:id', function (req, res) {
-	//res.send('Asking for todo with Id of ' +req.params.id );
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo;
-
-	todos.forEach(function (todo) {
-		if (todoId === todo.id) {
-			matchedTodo = todo;
-		}
-	});
+	var matchedTodo = _.findWhere(todos, {id: todoId});
 
 	if (matchedTodo) {
 		res.json(matchedTodo);
@@ -60,7 +30,42 @@ app.get('/todos/:id', function (req, res) {
 	}
 });
 
+// POST /todos
+app.post('/todos', function (req, res) {
+	var body = req.body;
+
+	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+		return res.status(400).send();
+	}
+
+	// add id field
+	body.id = todoNextId++;
+
+	// push body into array
+	todos.push(body);
+	
+	res.json(body);
+});
+
 
 app.listen(PORT, function () {
 	console.log('Express listening on port ' + PORT + '!');
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
