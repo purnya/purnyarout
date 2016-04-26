a59cef6808108dba287443f7c5274aee0a7e6b11
Purnya Rout <purnya.rout09@gmail.com>
2016-04-26 20:22:10 +0530

    space

--- playground/basic-sqlite-database-validation.js
+++ playground/basic-sqlite-database-validation.js
@@ -4,6 +4,7 @@ var sequelize = new Sequelize(undefined, undefined, undefined, {
 	'storage': __dirname + '/basic-sqlite-database.sqlite'
 });
 
+
 var Todo = sequelize.define('todo', {
 	description: {
 		type: Sequelize.STRING,
